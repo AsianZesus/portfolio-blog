2451cf2a67f7118313c36893dc720aa2602f3ebc
Rohan Awal <awalrohan8@gmail.com>
2024-03-06 16:13:39 +0545

    Creating dark/light theme toggle - (not finished)

---
+++ main.js
@@ -0,0 +1,17 @@
+const themeToggle = document.querySelector("#theme-toggle");
+
+themeToggle.addEventListener("click", () => {
+  document.body.classList.contains("light-theme")
+    ? enableDarkMode()
+    : enableLightMode();
+});
+
+function enableDarkMode() {
+  document.body.classList.remove("light-theme");
+  document.body.classList.add("dark-theme");
+}
+
+function enableLightMode() {
+  document.body.classList.remove("dark-theme");
+  document.body.classList.add("light-theme");
+}
