83b61d3f0dceb6fa7b1842814c041c32ee43ade3
Rohan Awal <awalrohan8@gmail.com>
2024-03-08 13:32:33 +0545

    Dark/light theme toggle finsish

--- main.js
+++ main.js
@@ -9,9 +9,21 @@ themeToggle.addEventListener("click", () => {
 function enableDarkMode() {
   document.body.classList.remove("light-theme");
   document.body.classList.add("dark-theme");
+  themeToggle.setAttribute("aria-label", "Switch to light theme");
 }
 
 function enableLightMode() {
   document.body.classList.remove("dark-theme");
   document.body.classList.add("light-theme");
+  themeToggle.setAttribute("aria-label", "Switch to dark theme");
 }
+
+function setThemePreference() {
+  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    enableDarkMode();
+    return;
+  }
+  enableLightMode();
+}
+
+document.onload = setThemePreference();
