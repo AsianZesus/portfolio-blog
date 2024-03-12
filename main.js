3a84b267733d5fee8f23fe75f72b4a1b5c2d5f73
Rohan Awal <awalrohan8@gmail.com>
2024-03-12 18:51:27 +0545

    testing JSON

--- main.js
+++ main.js
@@ -38,3 +38,51 @@ function time() {
   }, 1000);
 }
 window.onload = time;
+
+// Projects
+
+async function projects() {
+  const requestURL = "https://http://localhost:5173/data/projects.json";
+  const request = new Request(requestURL);
+
+  const response = await fetch(request);
+  const superHeroesText = await response.text();
+
+  const superHeroes = JSON.parse(superHeroesText);
+  projectsHeader(superHeroes);
+  projectsHeader(superHeroes);
+}
+
+function projectsHeader(obj) {
+  const header = document.querySelector("#test-project");
+  const myH1 = document.createElement("h1");
+  myH1.textContent = obj.year;
+  header.appendChild(myH1);
+
+  const myPara = document.createElement("p");
+  myPara.textContent = `Hometown: ${obj.name}`;
+  header.appendChild(myPara);
+}
+
+function populateProjects(obj) {
+  const section = document.querySelector(".test-section");
+  const heroes = obj.projects;
+
+  for (const hero of heroes) {
+    const myH2 = document.createElement("h2");
+
+    myH2.textContent = hero.name;
+
+    const superPowers = hero.year;
+
+    for (const power of superPowers) {
+      const myh3 = document.createElement("h3");
+
+      myh3.textContent = power;
+    }
+
+    section.appendChild(myH2);
+  }
+}
+
+projects();
